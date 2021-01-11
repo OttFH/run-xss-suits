09feacc6efc1726fd5698e9d13239355a109abc6
Clemens Ott <48800240+OttFH@users.noreply.github.com>
2021-01-11 18:50:43 +0100

    added eval url

--- XSpear/commands.sh
+++ XSpear/commands.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-XSpear -u http://xss-suite-test-site.herokuapp.com/pages/simple_tag.php?xss=234 -o json -v 0 > /logs/XSpear.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/simple_tag.php?xss=234 -o json -v 0 > /logs/XSpear_simple_tag.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/simple_value.php?xss=234 -o json -v 0 > /logs/XSpear_simple_value.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/simple_comment.php?xss=234 -o json -v 0 > /logs/XSpear_simple_comment.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/simple_innerHTML.php?xss=234 -o json -v 0 > /logs/XSpear_simple_innerHTML.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/simple_innerText.php?xss=234 -o json -v 0 > /logs/XSpear_simple_innerText.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/simple_eval.php?xss=234 -o json -v 0 > /logs/XSpear_simple_eval.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/escaping_tag.php?xss=234 -o json -v 0 > /logs/XSpear_escaping_tag.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/escaping_value.php?xss=234 -o json -v 0 > /logs/XSpear_escaping_value.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/escaping_comment.php?xss=234 -o json -v 0 > /logs/XSpear_escaping_comment.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/script_single_quote_text.php?xss=234 -o json -v 0 > /logs/XSpear_script_single_quote_text.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/script_double_quote_text.php?xss=234 -o json -v 0 > /logs/XSpear_script_double_quote_text.json
+XSpear -u https://xss-suite-test-site.herokuapp.com/pages/script_backticks_quote_text.php?xss=234 -o json -v 0 > /logs/XSpear_script_backticks_quote_text.json
