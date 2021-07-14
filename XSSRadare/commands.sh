d914b449c556778596ccf58eaade13a492f0f5f1
Clemens Ott <48800240+OttFH@users.noreply.github.com>
2021-07-14 16:31:00 +0200

    updated results

--- XSSRadare/commands.sh
+++ XSSRadare/commands.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_tag.php?xss=234 --full --stop # > /logs/xssradare_php_simple_tag.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_value.php?xss=234 --full --stop # > /logs/xssradare_php_simple_value.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_comment.php?xss=234 --full --stop # > /logs/xssradare_php_simple_comment.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_innerHTML.php?xss=234 --full --stop # > /logs/xssradare_php_simple_innerHTML.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/delayed_innerHTML.php?xss=234 --full --stop # > /logs/xssradare_php_delayed_innerHTML.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_innerText.php?xss=234 --full --stop # > /logs/xssradare_php_simple_innerText.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_eval.php?xss=234 --full --stop # > /logs/xssradare_php_simple_eval.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_tag.php?xss=234 --full --stop # > /logs/xssradare_php_escaping_tag.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_value.php?xss=234 --full --stop # > /logs/xssradare_php_escaping_value.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_comment.php?xss=234 --full --stop # > /logs/xssradare_php_escaping_comment.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_onload.php?xss=234 --full --stop # > /logs/xssradare_php_escaping_onload.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/script_single_quote_text.php?xss=234 --full --stop # > /logs/xssradare_php_script_single_quote_text.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/script_double_quote_text.php?xss=234 --full --stop # > /logs/xssradare_php_script_double_quote_text.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/script_backticks_quote_text.php?xss=234 --full --stop # > /logs/xssradare_php_script_backticks_quote_text.log
-python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/script_json_encode.php?xss=234 --full --stop # > /logs/xssradare_php_script_json_encode.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_tag.php?xss=234 --full --stop > /logs/xssradare_php_simple_tag.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_value.php?xss=234 --full --stop > /logs/xssradare_php_simple_value.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_comment.php?xss=234 --full --stop > /logs/xssradare_php_simple_comment.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_innerHTML.php?xss=234 --full --stop > /logs/xssradare_php_simple_innerHTML.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/delayed_innerHTML.php?xss=234 --full --stop > /logs/xssradare_php_delayed_innerHTML.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_innerText.php?xss=234 --full --stop > /logs/xssradare_php_simple_innerText.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_eval.php?xss=234 --full --stop > /logs/xssradare_php_simple_eval.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_tag.php?xss=234 --full --stop > /logs/xssradare_php_escaping_tag.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_value.php?xss=234 --full --stop > /logs/xssradare_php_escaping_value.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_comment.php?xss=234 --full --stop > /logs/xssradare_php_escaping_comment.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_onload.php?xss=234 --full --stop > /logs/xssradare_php_escaping_onload.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/script_single_quote_text.php?xss=234 --full --stop > /logs/xssradare_php_script_single_quote_text.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/script_double_quote_text.php?xss=234 --full --stop > /logs/xssradare_php_script_double_quote_text.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/script_backticks_quote_text.php?xss=234 --full --stop > /logs/xssradare_php_script_backticks_quote_text.log
+python3 XSSRadare.py -u https://xss-suite-test-site.herokuapp.com/pages/script_json_encode.php?xss=234 --full --stop > /logs/xssradare_php_script_json_encode.log
 
 
-python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/simpleTag?xss=234 --full --stop # > /logs/xssradare_react_simpleTag.log
-python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/simpleValue?xss=234 --full --stop # > /logs/xssradare_react_simpleValue.log
-python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/simpleComment?xss=234 --full --stop # > /logs/xssradare_react_simpleComment.log
-python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/escapingTag?xss=234 --full --stop # > /logs/xssradare_react_escapingTag.log
-python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/escapingValue?xss=234 --full --stop # > /logs/xssradare_react_escapingValue.log
-python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/escapingComment?xss=234 --full --stop # > /logs/xssradare_react_escapingComment.log
-python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/escapingHref?xss=234 --full --stop # > /logs/xssradare_react_escapingHref.log
-python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/escapingOnload?xss=234 --full --stop # > /logs/xssradare_react_escapingOnload.log
+python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/simpleTag?xss=234 --full --stop > /logs/xssradare_react_simpleTag.log
+python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/simpleValue?xss=234 --full --stop > /logs/xssradare_react_simpleValue.log
+python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/simpleComment?xss=234 --full --stop > /logs/xssradare_react_simpleComment.log
+python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/escapingTag?xss=234 --full --stop > /logs/xssradare_react_escapingTag.log
+python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/escapingValue?xss=234 --full --stop > /logs/xssradare_react_escapingValue.log
+python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/escapingComment?xss=234 --full --stop > /logs/xssradare_react_escapingComment.log
+python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/escapingHref?xss=234 --full --stop > /logs/xssradare_react_escapingHref.log
+python3 XSSRadare.py -u https://react-xss-suite-test-site.herokuapp.com/escapingOnload?xss=234 --full --stop > /logs/xssradare_react_escapingOnload.log
 
 
-python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/simpleTag?xss=234 --full --stop # > /logs/xssradare_vue_simpleTag.log
-python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/simpleValue?xss=234 --full --stop # > /logs/xssradare_vue_simpleValue.log
-python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/simpleComment?xss=234 --full --stop # > /logs/xssradare_vue_simpleComment.log
-python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingTag?xss=234 --full --stop # > /logs/xssradare_vue_escapingTag.log
-python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingValue?xss=234 --full --stop # > /logs/xssradare_vue_escapingValue.log
-python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingComment?xss=234 --full --stop # > /logs/xssradare_vue_escapingComment.log
-python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingHref?xss=234 --full --stop # > /logs/xssradare_vue_escapingHref.log
-python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingOnLoad?xss=234 --full --stop # > /logs/xssradare_vue_escapingOnLoad.log
+python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/simpleTag?xss=234 --full --stop > /logs/xssradare_vue_simpleTag.log
+python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/simpleValue?xss=234 --full --stop > /logs/xssradare_vue_simpleValue.log
+python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/simpleComment?xss=234 --full --stop > /logs/xssradare_vue_simpleComment.log
+python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingTag?xss=234 --full --stop > /logs/xssradare_vue_escapingTag.log
+python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingValue?xss=234 --full --stop > /logs/xssradare_vue_escapingValue.log
+python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingComment?xss=234 --full --stop > /logs/xssradare_vue_escapingComment.log
+python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingHref?xss=234 --full --stop > /logs/xssradare_vue_escapingHref.log
+python3 XSSRadare.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingOnload?xss=234 --full --stop > /logs/xssradare_vue_escapingOnLoad.log
 
 
-python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/simpleTag?xss=234 --full --stop # > /logs/xssradare_angular_simpleTag.log
-python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/simpleValue?xss=234 --full --stop # > /logs/xssradare_angular_simpleValue.log
-python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/simpleComment?xss=234 --full --stop # > /logs/xssradare_angular_simpleComment.log
-python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/sanitizedTag?xss=234 --full --stop # > /logs/xssradare_angular_sanitizedTag.log
-python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/sanitizedValue?xss=234 --full --stop # > /logs/xssradare_angular_sanitizedValue.log
-python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/sanitizedHref?xss=234 --full --stop # > /logs/xssradare_angular_sanitizedHref.log
-python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/escapingTag?xss=234 --full --stop # > /logs/xssradare_angular_escapingTag.log
-python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/escapingValue?xss=234 --full --stop # > /logs/xssradare_angular_escapingValue.log
-python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/escapingOnLoad?xss=234 --full --stop # > /logs/xssradare_angular_escapingOnLoad.log
+python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/simpleTag?xss=234 --full --stop > /logs/xssradare_angular_simpleTag.log
+python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/simpleValue?xss=234 --full --stop > /logs/xssradare_angular_simpleValue.log
+python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/simpleComment?xss=234 --full --stop > /logs/xssradare_angular_simpleComment.log
+python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/sanitizedTag?xss=234 --full --stop > /logs/xssradare_angular_sanitizedTag.log
+python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/sanitizedValue?xss=234 --full --stop > /logs/xssradare_angular_sanitizedValue.log
+python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/sanitizedHref?xss=234 --full --stop > /logs/xssradare_angular_sanitizedHref.log
+python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/escapingTag?xss=234 --full --stop > /logs/xssradare_angular_escapingTag.log
+python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/escapingValue?xss=234 --full --stop > /logs/xssradare_angular_escapingValue.log
+python3 XSSRadare.py -u https://angular-xss-suite-test-site.herokuapp.com/escapingOnload?xss=234 --full --stop > /logs/xssradare_angular_escapingOnLoad.log
--- XSStrike/commands.sh
+++ XSStrike/commands.sh
@@ -1,4 +1,48 @@
 #!/bin/bash
-python xsstrike.py --skip --seeds get_url_list.txt --file-log-level GOOD --log-file /logs/xsstrike/xsstrike_get.log
-# python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/post_simple_tag.php --data "xss=234" --file-log-level GOOD --log-file /logs/xsstrike/xsstrike_post.log <<< "n"
-# echo ""
+
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_tag.php?xss=234 > /logs/xsstrike_php_simple_tag.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_value.php?xss=234 > /logs/xsstrike_php_simple_value.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_comment.php?xss=234 > /logs/xsstrike_php_simple_comment.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_innerHTML.php?xss=234 > /logs/xsstrike_php_simple_innerHTML.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/delayed_innerHTML.php?xss=234 > /logs/xsstrike_php_delayed_innerHTML.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_innerText.php?xss=234 > /logs/xsstrike_php_simple_innerText.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/simple_eval.php?xss=234 > /logs/xsstrike_php_simple_eval.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_tag.php?xss=234 > /logs/xsstrike_php_escaping_tag.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_value.php?xss=234 > /logs/xsstrike_php_escaping_value.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_comment.php?xss=234 > /logs/xsstrike_php_escaping_comment.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/escaping_onload.php?xss=234 > /logs/xsstrike_php_escaping_onload.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/script_single_quote_text.php?xss=234 > /logs/xsstrike_php_script_single_quote_text.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/script_double_quote_text.php?xss=234 > /logs/xsstrike_php_script_double_quote_text.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/script_backticks_quote_text.php?xss=234 > /logs/xsstrike_php_script_backticks_quote_text.log
+echo 'N\n' | python xsstrike.py -u https://xss-suite-test-site.herokuapp.com/pages/script_json_encode.php?xss=234 > /logs/xsstrike_php_script_json_encode.log
+
+
+echo 'N\n' | python xsstrike.py -u https://react-xss-suite-test-site.herokuapp.com/simpleTag?xss=234 > /logs/xsstrike_react_simpleTag.log
+echo 'N\n' | python xsstrike.py -u https://react-xss-suite-test-site.herokuapp.com/simpleValue?xss=234 > /logs/xsstrike_react_simpleValue.log
+echo 'N\n' | python xsstrike.py -u https://react-xss-suite-test-site.herokuapp.com/simpleComment?xss=234 > /logs/xsstrike_react_simpleComment.log
+echo 'N\n' | python xsstrike.py -u https://react-xss-suite-test-site.herokuapp.com/escapingTag?xss=234 > /logs/xsstrike_react_escapingTag.log
+echo 'N\n' | python xsstrike.py -u https://react-xss-suite-test-site.herokuapp.com/escapingValue?xss=234 > /logs/xsstrike_react_escapingValue.log
+echo 'N\n' | python xsstrike.py -u https://react-xss-suite-test-site.herokuapp.com/escapingComment?xss=234 > /logs/xsstrike_react_escapingComment.log
+echo 'N\n' | python xsstrike.py -u https://react-xss-suite-test-site.herokuapp.com/escapingHref?xss=234 > /logs/xsstrike_react_escapingHref.log
+echo 'N\n' | python xsstrike.py -u https://react-xss-suite-test-site.herokuapp.com/escapingOnload?xss=234 > /logs/xsstrike_react_escapingOnload.log
+
+
+echo 'N\n' | python xsstrike.py -u https://vue-xss-suite-test-site.herokuapp.com/simpleTag?xss=234 > /logs/xsstrike_vue_simpleTag.log
+echo 'N\n' | python xsstrike.py -u https://vue-xss-suite-test-site.herokuapp.com/simpleValue?xss=234 > /logs/xsstrike_vue_simpleValue.log
+echo 'N\n' | python xsstrike.py -u https://vue-xss-suite-test-site.herokuapp.com/simpleComment?xss=234 > /logs/xsstrike_vue_simpleComment.log
+echo 'N\n' | python xsstrike.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingTag?xss=234 > /logs/xsstrike_vue_escapingTag.log
+echo 'N\n' | python xsstrike.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingValue?xss=234 > /logs/xsstrike_vue_escapingValue.log
+echo 'N\n' | python xsstrike.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingComment?xss=234 > /logs/xsstrike_vue_escapingComment.log
+echo 'N\n' | python xsstrike.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingHref?xss=234 > /logs/xsstrike_vue_escapingHref.log
+echo 'N\n' | python xsstrike.py -u https://vue-xss-suite-test-site.herokuapp.com/escapingOnload?xss=234 > /logs/xsstrike_vue_escapingOnLoad.log
+
+
+echo 'N\n' | python xsstrike.py -u https://angular-xss-suite-test-site.herokuapp.com/simpleTag?xss=234 > /logs/xsstrike_angular_simpleTag.log
+echo 'N\n' | python xsstrike.py -u https://angular-xss-suite-test-site.herokuapp.com/simpleValue?xss=234 > /logs/xsstrike_angular_simpleValue.log
+echo 'N\n' | python xsstrike.py -u https://angular-xss-suite-test-site.herokuapp.com/simpleComment?xss=234 > /logs/xsstrike_angular_simpleComment.log
+echo 'N\n' | python xsstrike.py -u https://angular-xss-suite-test-site.herokuapp.com/sanitizedTag?xss=234 > /logs/xsstrike_angular_sanitizedTag.log
+echo 'N\n' | python xsstrike.py -u https://angular-xss-suite-test-site.herokuapp.com/sanitizedValue?xss=234 > /logs/xsstrike_angular_sanitizedValue.log
+echo 'N\n' | python xsstrike.py -u https://angular-xss-suite-test-site.herokuapp.com/sanitizedHref?xss=234 > /logs/xsstrike_angular_sanitizedHref.log
+echo 'N\n' | python xsstrike.py -u https://angular-xss-suite-test-site.herokuapp.com/escapingTag?xss=234 > /logs/xsstrike_angular_escapingTag.log
+echo 'N\n' | python xsstrike.py -u https://angular-xss-suite-test-site.herokuapp.com/escapingValue?xss=234 > /logs/xsstrike_angular_escapingValue.log
+echo 'N\n' | python xsstrike.py -u https://angular-xss-suite-test-site.herokuapp.com/escapingOnload?xss=234 > /logs/xsstrike_angular_escapingOnLoad.log
